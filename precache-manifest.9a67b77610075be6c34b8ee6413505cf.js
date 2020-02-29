844b56cf39b922f223f566aeff7d0cb9b7cc9446
jayaputra <joyaojahoy@yahoo.com>
2020-02-29 11:43:26 +0700

    Updates

---
+++ precache-manifest.9a67b77610075be6c34b8ee6413505cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bbf4e3b590c84a6001f665294c3959c",
+    "url": "/react2/index.html"
+  },
+  {
+    "revision": "00f90ec8c98155389faf",
+    "url": "/react2/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "54032d983f32a8703a81",
+    "url": "/react2/static/js/2.ced51817.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react2/static/js/2.ced51817.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00f90ec8c98155389faf",
+    "url": "/react2/static/js/main.a447753d.chunk.js"
+  },
+  {
+    "revision": "e936909689a464d424c8",
+    "url": "/react2/static/js/runtime-main.1139e415.js"
+  },
+  {
+    "revision": "d3b4a514dfb172f6ac1817f5bdb974ad",
+    "url": "/react2/static/media/bg-masthead.d3b4a514.jpg"
+  }
+]);
